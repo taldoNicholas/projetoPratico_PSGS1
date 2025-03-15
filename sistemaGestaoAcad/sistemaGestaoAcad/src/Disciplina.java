c098b47e92e18153dde03a7e28099849f9d25caa
Nicholas Rangel <nicholashenry2014@gmail.com>
2025-03-15 13:45:38 -0300

    classe pessoa e discilpina

---
+++ sistemaGestaoAcad/sistemaGestaoAcad/src/Disciplina.java
@@ -0,0 +1,11 @@
+public class Disciplina {
+    int codDisciplina;
+    String nomeDisciplina;
+    String nomeCurso;
+
+    public Disciplina(int codDisciplina, String nomeDisciplina, String nomeCurso){
+        this.codDisciplina = codDisciplina;
+        this.nomeDisciplina = nomeDisciplina;
+        this.nomeCurso = nomeCurso;
+    }
+}
